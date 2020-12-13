b35ee310a240d79d23284192fa9379989e099bd4
sudaaiko <aiko05.hello@gmail.com>
2020-12-13 16:09:59 +0900

    [finish]chapter04

---
+++ Java_Pra2/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "Aiko Suda";
+		System.out.println(name);
+
+		name = "こんにちは！";
+		System.out.println(name);
+
+		final int LUCKY_NUMBER = 2;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
