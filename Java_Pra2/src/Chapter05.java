e6ee410076281339485fd4633d8dbf5c1dbbfec8
sudaaiko <aiko05.hello@gmail.com>
2020-12-13 19:44:36 +0900

    [finish]chapter05

---
+++ Java_Pra2/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long num1;
+		double num2;
+		boolean boolean1;
+		num1 = 20000;
+		num2 = 2.3;
+		boolean1 = true;
+
+		Integer integer1 = 3;
+		int[] array = new int[] {2,3,4,5,6,3};
+
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(boolean1);
+		System.out.println(integer1);
+		System.out.println(array[3]);
+	}
+
+}
